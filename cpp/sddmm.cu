e09277481645e8869b11ee5d52eba69551bd6dc5
rhdong <rhdong2017@gmail.com>
2023-12-29 13:51:18 -0800

    d5

--- cpp/sddmm.cu
+++ cpp/sddmm.cu
@@ -263,7 +263,10 @@ int test_main(SDDMMBenchParams& params, Timer<double>& timer)
 
 int main(void)
 {
-  std::vector<SDDMMBenchParams> cases{{1024 * 1024, 128, 1024, 0.249, 1.0f, 0.0f},
+  std::vector<SDDMMBenchParams> cases{{1024 * 1024, 128, 1024, 0.01, 1.0f, 0.0f},
+                                      {1024 * 1024, 128, 1024, 0.1, 1.0f, 0.0f},
+                                      {1024 * 1024, 128, 1024, 0.2, 1.0f, 0.0f},
+                                      {1024 * 1024, 128, 1024, 0.249, 1.0f, 0.0f},
                                       {1024 * 1024, 128, 1024, 0.251, 1.0f, 0.0f}};
 
   auto timer = Timer<double>();
