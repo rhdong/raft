c4288d84053252d3f8753b0717cf5c40c5841e48
hrong <hrong@nvidia.com>
2024-01-04 11:50:12 -0800

    d1

--- cpp/sddmm.cu
+++ cpp/sddmm.cu
@@ -193,7 +193,7 @@ void test_main(BenchParams& params, Timer<double>& timer)
 
   // Perpare C and test
   // The first sparsity is only for warmup.
-  std::vector<float> sparsity_list = {0.001, 0.5};
+  std::vector<float> sparsity_list = {0.001, 0.01, 0.1, 0.19, 0.2, 0.5};
   size_t pre_buffer_size           = 0;
   bool warmup                      = true;
   int times                        = 3;
@@ -329,9 +329,9 @@ int main(void)
     //     {1024 * 1024, 128, 1024, 0.01, 1.0f, 0.0f, true, true},
     //     {1024 * 1024, 1024, 1024, 0.01, 1.0f, 0.0f, true, true},
         {1024 * 1024, 1024, 2 * 1024, 0.01, 1.0f, 0.0f, true, true}
-    //     {1024 * 1024, 128, 1024, 0.01, 1.0f, 0.0f, false, false},
-    //     {1024 * 1024, 1024, 1024, 0.01, 1.0f, 0.0f, false, false},
-//     {1024 * 1024, 1024, 2 * 1024, 0.01, 1.0f, 0.0f, false, false},
+        {1024 * 1024, 128, 1024, 0.01, 1.0f, 0.0f, false, false},
+        {1024 * 1024, 1024, 1024, 0.01, 1.0f, 0.0f, false, false},
+        {1024 * 1024, 1024, 2 * 1024, 0.01, 1.0f, 0.0f, false, false},
     };
 
   auto timer = Timer<double>();
