5184ec474b4627fbb22b156021a8adf0bf1b12ca
rhdong <hrong@nvidia.com>
2024-02-22 19:04:46 -0800

    d5

--- cpp/include/raft/sparse/convert/detail/bitmap_to_csr.cuh
+++ cpp/include/raft/sparse/convert/detail/bitmap_to_csr.cuh
@@ -211,7 +211,7 @@ RAFT_KERNEL __launch_bounds__(fill_indices_by_rows_tpb)
 
       for (int i = 0; i < BITS_PER_BITMAP; i++) {
         if(l_bitmap & (ONE << i)) {
-          // indices[indptr[row] + g_sum + l_sum] = offset  + lane_id * BITS_PER_BITMAP - (s_bit % BITS_PER_BITMAP) + i;
+          indices[indptr[row] + g_sum + l_sum] = offset  + lane_id * BITS_PER_BITMAP - (s_bit % BITS_PER_BITMAP) + i;
           l_sum++;
 //           printf("row=%d, lane_id=%d, indptr[row]=%d, g_sum=%d, l_sum=%d, offset=%d, i=%d, l_bitmap=%d, s_bit=%d, r=%d\n",
 //                  row, lane_id, indptr[row], g_sum, l_sum, offset, i, l_bitmap, s_bit,
