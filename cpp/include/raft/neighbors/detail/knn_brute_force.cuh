e860dfae0ec09fda82e8f473222d0bd3e61f1021
hrong <hrong@nvidia.com>
2024-05-02 18:27:31 -0700

    d4

--- cpp/include/raft/neighbors/detail/knn_brute_force.cuh
+++ cpp/include/raft/neighbors/detail/knn_brute_force.cuh
@@ -556,12 +556,16 @@ void brute_force_search(
                                          query_norms ? query_norms->data_handle() : nullptr);
 }
 
-template <typename T>
-__global__ void dump_array_kernel(T* array, int size, const char* name)
+template <typename T, typename IdxT>
+__global__ void dump_array_kernel(T* array, IdxT size, const char* name)
 {
   printf("device: %s\n", name);
-  for (int i = 0; i < size; i++) {
-    printf("%d, ", array[i]);
+  for (IdxT i = 0; i < size; i++) {
+  	if constexpr (std::is_integral_v<T>::value){
+      printf("%d, ", array[i]);
+    } else {
+      printf("%f, ", array[i]);
+	}
   }
   printf("\n");
 }
@@ -628,8 +632,8 @@ void brute_force_search(
     csr.get_elements().data(), csr.structure_view());
   std::optional<raft::device_vector_view<const IdxT, IdxT>> no_opt = std::nullopt;
   raft::sparse::matrix::select_k(res, const_csr_view, no_opt, distances, neighbors, true, true);
-  dump_array_kernel<<<1, 1, 0, stream>>>(neighbors.data_handle(), neighbors, "neighbors");
-  dump_array_kernel<<<1, 1, 0, stream>>>(distances.data_handle(), distances, "distances");
+  dump_array_kernel<<<1, 1, 0, stream>>>(neighbors.data_handle(), IdxT(neighbors.size()), "neighbors");
+  dump_array_kernel<<<1, 1, 0, stream>>>(distances.data_handle(), IdxT(distances.size()), "distances");
 
   // post process
   /*auto metric    = idx.metric();
